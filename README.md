ff98e2d4a50e5b4acc1a333d600b8f400cd64652
nices96 <nices96@gmail.com>
2016-10-21 22:51:51 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # scouter-plugin-server-dump-thread
-Scouter server plugin to dump ThreadDumps
+### Scouter server plugin to dump ThreadDumps
+
+- 본 프로젝트는 스카우터 서버 플러그인으로써 java agent의 ProcessCpu가 정해진 시간동안 임계치를 초과할 경우 스레드 덤프 파일을 생성한다.
+
+### Properties (스카우터 서버 설치 경로 하위의 conf/scouter.conf)
+* **_ext\_plugin\_dump\_thread\_enable_** : Dump 기능 사용 여부 (true / false) - 기본 값은 false
+* **_ext\_plugin\_dump\_thread\_debug_** : 로깅 여부 - 기본 값은 false
+* **_ext\_plugin\_dump\_thread\_threshhold\_cpu_** : ProcessCpu의 임계치 값(%)
+* **_ext\_plugin\_dump\_thread\_cpu\_high\_duration_** : ProcessCpu가 임계치를 초과하여 유지해야 하는 시간(초)
+* **_ext\_plugin\_dump\_thread\_dump\_interval_** : Thread Dump 수행 주기(초)
+* **_ext\_plugin\_dump\_thread\_dump\_count_** : ProcessCpu가 임계치를 초과하여 정해진 시간동안 유지될 때 최대로 생성되는 덤프 수
+
+* Example
+```
+# External Interface (Email)
+ext_plugin_dump_thread_enable=true
+ext_plugin_dump_thread_debug=true
+ext_plugin_dump_thread_threshhold_cpu=80
+ext_plugin_dump_thread_cpu_high_duration=300
+ext_plugin_dump_thread_dump_interval=5
+ext_plugin_dump_thread_dump_count=3
+```
+
+### Dependencies
+* Project
+    - scouter.common
+    - scouter.server
+    
+### Build & Deploy
+* Build
+    - mvn install을 수행한다.
+    
+* Deploy
+    - 빌드 후 프로젝트 하위의 target 디렉토리에 scouter-plugin-server-dump-thread-0.0.1.jar 파일을 복사하여 스카우터 서버 설치 경로 하위의 lib/ 폴더에 저장한다.
---
+++ src/main/java/scouter/plugin/server/dump/thread/ThreadDumpPlugin.java
@@ -0,0 +1,90 @@
+package scouter.plugin.server.dump.thread;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import scouter.lang.TimeTypeEnum;
+import scouter.lang.counters.CounterConstants;
+import scouter.lang.pack.MapPack;
+import scouter.lang.pack.ObjectPack;
+import scouter.lang.pack.PerfCounterPack;
+import scouter.lang.plugin.PluginConstants;
+import scouter.lang.plugin.annotation.ServerPlugin;
+import scouter.net.RequestCmd;
+import scouter.server.Configure;
+import scouter.server.CounterManager;
+import scouter.server.Logger;
+import scouter.server.core.AgentManager;
+import scouter.server.netio.AgentCall;
+import scouter.util.HashUtil;
+
+public class ThreadDumpPlugin {
+	
+	private static Map<Integer, Long> cpuHighTimeMap = new HashMap<Integer, Long>();
+	private static Map<Integer, Long> dumpTimeMap = new HashMap<Integer, Long>();
+	private static Map<Integer, Integer> dumpCountMap = new HashMap<Integer, Integer>();
+
+    final Configure conf = Configure.getInstance();
+
+    @ServerPlugin(PluginConstants.PLUGIN_SERVER_COUNTER)
+    public void counter(PerfCounterPack pack) {
+        String objName = pack.objName;
+        int objHash = HashUtil.hash(objName);
+        String objType = null;
+        String objFamily = null;
+
+        try {
+	        if (AgentManager.getAgent(objHash) != null) {
+	        	objType = AgentManager.getAgent(objHash).objType;
+	        }
+	        
+	        if (objType != null) {
+	        	objFamily = CounterManager.getInstance().getCounterEngine().getObjectType(objType).getFamily().getName();
+	        }
+	        
+	        // objFamily가 javaee인 경우
+	        if (CounterConstants.FAMILY_JAVAEE.equals(objFamily)) {
+	        	if (pack.timetype == TimeTypeEnum.REALTIME && pack.data.getFloat(CounterConstants.PROC_CPU) > 0f) {
+	            	float maxCpu = new Float(conf.getValue("ext_plugin_dump_thread_threshhold_cpu", "80.0"));
+	
+	            	// CPU 임계치를 초과한 경우
+	            	if (pack.data.getFloat(CounterConstants.PROC_CPU) >= maxCpu) {
+	            		long currentTime = System.currentTimeMillis();
+	            		long firstTime = cpuHighTimeMap.get(objHash) == null ? currentTime : cpuHighTimeMap.get(objHash);
+	            		long lastTime = dumpTimeMap.get(objHash) == null ? 0L : dumpTimeMap.get(objHash);
+	            		int count = dumpCountMap.get(objHash) == null ? 0 : dumpCountMap.get(objHash);
+	                	
+	            		// CPU 임계치 초과 상태가 300초동안 지속될 경우 
+	            		if (currentTime - firstTime > (1000 * conf.getInt("ext_plugin_dump_thread_cpu_high_duration", 300))) {
+		            		// 5초 단위로 3회 스레드 덤프를 수행한다.
+		            		if (currentTime - lastTime > (1000 * conf.getInt("ext_plugin_dump_thread_dump_interval", 5)) && count < conf.getInt("ext_plugin_dump_thread_dump_count", 3)) {
+		                		println("[" + objName + "] Exceed the limit of cpu's threshhold(" + maxCpu + "). ThreadDump will be generated.");
+		                    	ObjectPack objectPack = AgentManager.getAgent(objHash);
+		                    	MapPack mapPack = new MapPack();
+		                    	mapPack.put("objHash", objHash);
+		                        AgentCall.call(objectPack, RequestCmd.TRIGGER_THREAD_DUMP, mapPack);
+		                        
+		                        dumpTimeMap.put(objHash, currentTime);
+		                        dumpCountMap.put(objHash, ++count);
+		            		}
+	            		}
+	            		
+	            		cpuHighTimeMap.put(objHash, firstTime);
+	            	} else {
+	            		cpuHighTimeMap.put(objHash, null);
+	            		dumpTimeMap.put(objHash, 0L);
+	            		dumpCountMap.put(objHash, 0);
+	            	}
+	        	}
+	    	}
+        } catch (Exception e) {
+        	Logger.printStackTrace(e);
+        }
+    }
+
+    private void println(Object o) {
+        if (conf.getBoolean("ext_plugin_dump_thread_debug", false)) {
+            Logger.println(o);
+        }
+    }
+}
